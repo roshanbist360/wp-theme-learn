e2c11c280a894945f3996bc1f9fe72e4c29b91df
Roshan Bist <roshanbist900@gmail.com>
2023-06-01 13:36:11 +0545

    custom-post-type

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
 <?php get_header(); ?>
 <?php
-  while (have_posts()){
-    the_post();
+    $args = array(
+      'post_type' => 'post', //if we have to show posts of project then we will replace post by project here
+      'posts_per_page' => 2, //similarly we have many functions like this
+    );
+    $blogposts = new WP_Query($args);
+  while ($blogposts->have_posts()){
+    $blogposts->the_post();
   ?>
    <a href="<?php the_permalink(); ?>">
       <h3><?php  the_title(); ?></h3>
--- single.php
+++ single.php
@@ -1,12 +1,11 @@
 <?php get_header(); ?>
   <?php 
      while (have_posts()){
-       the_post();
-    
+     the_post();  
   ?>
 
   <h3><?php  the_title(); ?></h3>
-  Posted by <?php the_author(); ?>
+  Posted by:  <?php the_author(); ?>
   <img src="<?php echo get_the_post_thumbnail_url(get_the_ID()); ?>" alt=""/>
   <?php the_content();
   comment_form();
