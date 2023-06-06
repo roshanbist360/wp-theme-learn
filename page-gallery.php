bd92e09dfc3523daf7a15d5e4f2b2d2556500acf
Roshan Bist <roshanbist900@gmail.com>
2023-06-06 16:07:33 +0545

    title-for-gallery-images-added-in-gallery-page

--- page-gallery.php
+++ page-gallery.php
@@ -4,26 +4,30 @@
     <div class="container">
         <div class="row myFancyGallery">
         <?php
-// Get the post ID from the query parameter
-$post_id = isset($_GET['post_id']) ? absint($_GET['post_id']) : 0;
+        // Get the post ID from the query parameter
+        $post_id = isset($_GET['post_id']) ? absint($_GET['post_id']) : 0;
 
-// Retrieve the images associated with the post ID
-$gallery_images = get_post_gallery_images($post_id);
+        // Retrieve the images associated with the post ID
+        $gallery_images = get_post_gallery_images($post_id);
 
-if ($gallery_images) {
-    foreach ($gallery_images as $image_url) {
+        if ($gallery_images) {
+            $post_title = get_the_title($post_id);
+            ?>
+            <h2 class="text-center mb-5">Image Gallery For : <?php echo esc_html($post_title); ?></h2>
+            <?php
+            foreach ($gallery_images as $image_url) {
+                ?>
+                <div class="col-lg-2 col-md-3 col-4 fancy_item">
+                    <a href="<?php echo esc_url($image_url); ?>" data-fancybox="gallery" data-caption="Caption #1">
+                        <img src="<?php echo esc_url($image_url); ?>" alt="gallery" />
+                    </a>
+                </div>
+                <?php
+            }
+        } else {
+            echo '<p>No gallery images found.</p>';
+        }
         ?>
-        <div class="col-lg-2 col-md-3 col-4 fancy_item">
-            <a href="<?php echo esc_url($image_url); ?>" data-fancybox="gallery" data-caption="Caption #1">
-                <img src="<?php echo esc_url($image_url); ?>" alt="gallery" />
-            </a>
-        </div>
-        <?php
-    }
-} else {
-    echo '<p>No gallery images found.</p>';
-}
-?>
 
         </div>
     </div>
